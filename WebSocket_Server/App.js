f1563009f7c6f97e5a55769e945a02b1ac01353b
junochiu <ching.lun0216@gmail.com>
2020-11-23 19:35:24 +0800

    version for gamepad

--- WebSocket_Server/App.js
+++ WebSocket_Server/App.js
@@ -14,26 +14,27 @@ const http = require('https')
 const server = http.createServer(certOptions ,app)
       .listen( 3000,"0.0.0.0", ()=>{console.log('listening on 3000')})
 
-
-
-//open socket server on port 3000
-const io = require('socket.io')(server)
-
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname+'/index.html');
 });
 
+app.get('/script_for_aframe.js', (req, res) => {
+  res.sendFile(__dirname+'/script_for_aframe.js');
+});
 
+
+//--------------- socket ----------------
+//open socket server on port 3000
+const io = require('socket.io')(server)
 io.on('connection', socket => {
   console.log('success connect!');
 })
 
-
+/*------ read from sensor input ----------
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
 // open serial port
-const port = new SerialPort('/dev/input/js0', { baudRate: 9600  })
+const port = new SerialPort('/dev/ttyUSB0', { baudRate: 9600 })
 // feed port input to a line parser
 const parser = port.pipe(new Readline({delimiter: '\n'} ))
 // when parser get a line , send it to the socket
@@ -41,3 +42,40 @@ parser.on('data', line => {
     io.sockets.emit('update_data', line);
     //console.log(line);
 })
+*/
+
+//------ read from sensor joystick --------
+var gamepad = require("gamepad")
+gamepad.init()
+setInterval(gamepad.processEvents, 10);
+x_update = 0
+y_update = 0
+z_update = 0
+gamepad.on("move", function (id, axis, value) {
+    x = 0
+    y = 0
+    z = 0
+    var str = "{\"gyro\":{\"x\":"+x+",\"y\":"+y+",\"z\":"+z+"}}"
+    console.log(value);
+    // 搖桿左右
+    if(axis==3)
+    {
+        x = 0
+        y = -value*90
+        z = 0
+        str = "{\"gyro\":{\"x\":"+x+",\"y\":"+y+",\"z\":"+z+"}}"
+    }
+    // 搖桿上下
+    if(axis==4)
+    {
+        x = -value*90
+        y = 0
+        z = -value*90
+        str = "{\"gyro\":{\"x\":"+x+",\"y\":"+y+",\"z\":"+z+"}}"
+    }
+    x_update = x
+    y_update = y
+    z_update = z
+    io.sockets.emit('update_data', str);
+    console.log(str);
+});
---
+++ WebSocket_Server/script_for_aframe.js
@@ -0,0 +1,102 @@
+let Sensors = {humd:80,gyro:{x:0,y:0,z:0},acc:{x:0,y:0,z:0},temp:28};
+let reset = false;
+let BiasAng = {Bx:0,By:0,Bz:0};
+let angY = 0;
+
+      //reset the bias angle with enter key.
+const handleKeyDown = e =>
+{
+	if (e.key === 'Enter')
+	{
+	    reset = true;
+	    //console.log('reset');
+	}
+}
+
+window.addEventListener("keydown",handleKeyDown);
+
+//------------ socket for sensor------------
+var socket = io();
+socket.on("update_data", (data) =>
+{
+	let  diff
+	//console.log(data);
+	try {
+	    diff = JSON.parse(data);
+	}
+	catch(e){
+	    console.log("parse error");
+	}
+	Sensors =  ({...Sensors,...diff});
+	//console.log(diff);
+	//console.log(Sensors);
+	updateView();
+	if(reset){
+	    BiasAng = {
+		Bx: Sensors.gyro.x,
+		By: Sensors.gyro.y,
+		Bz: Sensors.gyro.z
+	    }
+	    reset = false;
+
+	}
+});
+//------------ A-frame ------------
+//const entityEl = document.querySelector("a-entity");
+const sceneEl = document.querySelector("a-scene");
+const translateController = sceneEl.querySelector("#translate-controller");
+const rotateController = sceneEl.querySelector("#rotate-controller");
+const rotateY = sceneEl.querySelector("#rotateY");
+const textController = sceneEl.querySelector("#text");
+
+//const viewController = entityEl.querySelector("#camera");
+//const box = sceneEl.querySelector("#box");
+
+
+const updateView = () => {
+
+	  //console.log(textController.getAttribute("text"));
+	  //console.log()
+	  //textController.setAttribute("text",`value:${"Temp:"+ Math.round(Sensors.temp * 100)/100+"\nHumid:"+(Math.round(Sensors.humd*100)/100)};color:#EF2D5E`);
+
+	  const Wx = Sensors.gyro.x - BiasAng.Bx;
+	  const Wy = Sensors.gyro.y - BiasAng.By;
+	  const Wz = Sensors.gyro.z - BiasAng.Bz;
+	  //const rot = rotateController.getAttribute("rotation");
+	  //console.log([rot.x,rot.y,rot.z]);
+
+	  /*============= for sensor ==============
+	  if(Wz>10)
+	      angY += 2;
+	  else if (Wz < -10)
+	      angY -= 2;
+	  else
+	      ;
+      =========================================*/
+
+	  //angY = -\Wz;
+
+	  rotateY.setAttribute("rotation",{x:0,y:angY,z:0});
+	  rotateController.setAttribute("rotation", { x: Wx, y:0 , z: -Wy });
+	  // box.setAttribute("rotation", { x: Wx, y: Wy, z: Wz });
+	  const { x, y, z } = translateController.getAttribute("position");
+	  //console.log([x,y,z]);
+	  let  dx1 = 0;
+	  let  dy1 = 0;
+	  let  dx2 = 0;
+	  let  dy2 = 0;
+	  let strip=10;
+	  if(Math.abs(Wy)>10){
+	      dx1 = strip*Math.cos(angY/180*Math.PI)*0.05 * Math.sign(Wy)*Math.min(3,Math.abs(Wy));
+	      dy1 = strip*Math.sin(angY/180*Math.PI)*0.05 * Math.sign(Wy)*Math.min(3,Math.abs(Wy));
+	  }
+	  if(Math.abs(Wx)>10){
+	      dx2 = strip*Math.cos(angY/180*Math.PI+Math.PI/2)*0.05 * Math.sign(Wx)*Math.min(3,Math.abs(Wx));
+	      dy2 = strip*Math.sin(angY/180*Math.PI+Math.PI/2)*0.05 * Math.sign(Wx)*Math.min(3,Math.abs(Wx));
+	  }
+	  translateController.setAttribute("position", {
+	      x: x + dx1 + dx2,
+	      y: -2.5,//-Pz
+	      z: z + dy1 + dy2
+	  });
+};
